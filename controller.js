5ea801927601c75bfc3c8e5bb7d63c8c352e17e0
Vang Xiong <vangx709@gmail.com>
2023-03-02 16:49:29 -0900

    fix open close API

--- controller.js
+++ controller.js
@@ -42,23 +42,18 @@ const onDelete = (todoToDelete) => {
   };
 };
 
-let dataSection = false;
 const closeAPI = () => {
   const button = document.getElementById("close-data-button");
   text = button.innerText;
 
   if ("Close" === text) {
-    // event.target.innerText = "Open";
     const apiButtons = document.getElementById("get-data");
     apiButtons.style.display = "none";
     button.innerText = "Open";
-    dataSection = true;
   } else {
-    // event.target.innerText = "Close";
     const apiButtons = document.getElementById("get-data");
     apiButtons.style.display = "block";
     button.innerText = "Close";
-    dataSection = false;
   }
 };
 
